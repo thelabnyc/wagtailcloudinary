07cfc90fd248fd11bdc6cb017c70e95ae002c3b6
will acheson <wacheson@thelabnyc.com>
2021-09-21 16:57:03 -0400

    need to import static differently to support django 3

--- wagtailcloudinary/sites.py
+++ wagtailcloudinary/sites.py
@@ -2,7 +2,7 @@ import cloudinary
 import cloudinary.api
 from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.templatetags.static import static
 from django.http import JsonResponse
 from django.template.loader import render_to_string
 from django.utils.html import format_html
