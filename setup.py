657402eff8242616a67350fe21ab7e15b75742e1
will acheson <wacheson@thelabnyc.com>
2021-09-23 12:46:50 -0400

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wagtailcloudinary',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(include=['wagtailcloudinary'], exclude=['sandbox']),
     install_requires=['cloudinary', 'wagtail'],
     include_package_data=True,
